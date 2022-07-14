4d0adf90f72d409b8d213e7a517b5d046f64d17e
moto95 <kobaesp1995@gmail.com>
2022-07-14 19:18:22 +0900

    バグの解消

--- kabuApp.py
+++ kabuApp.py
@@ -1,27 +1,26 @@
 import pandas as pd
-import altair as alt
 import yfinance as yf
+import altair as alt
 import streamlit as st
 
-st.title('米国株化可視化アプリ')
+st.title('米国株価可視化アプリ')
 
 st.sidebar.write("""
-  ## 株価
-
-  こちらは株価可視化ツールです。以下のオプションから表示日数を指定できます。
-  """)
+# GAFA株価
+こちらは株価可視化ツールです。以下のオプションから表示日数を指定できます。
+""")
 
 st.sidebar.write("""
-  ## 表示日数選択
-  """)
-# daysにはインタラクティブに変更した値が入ってくる
-days = st.sidebar.slider('日数', 1, 100, 20)
+## 表示日数選択
+""")
+
+days = st.sidebar.slider('日数', 1, 700, 20)
 
 st.write(f"""
-  ### 過去 **{days}日間** の株価
-  """)
+### 過去 **{days}日間** のGAFA株価
+""")
 
-# 株価をYahoo!ファイナンスから取ってきてデータフレームに入れる関数
+
 @st.cache
 def get_data(days, tickers):
     df = pd.DataFrame()
@@ -41,11 +40,11 @@ def get_data(days, tickers):
 st.sidebar.write("""
 ## 株価の範囲指定
 """)
+ymin, ymax = st.sidebar.slider(
+    '範囲を指定してください。',
+    0, 3000, (0, 200)
+)
 
-# ymin ymaxにはインタラクティブに変更した値が入ってくる
-ymin, ymax = st.sidebar.slider('範囲を指定ください', 0, 3000, (0, 3000))
-
-# 取得したい株のティッカー情報
 tickers = {
     'VYM': 'VYM',
     'VT': 'VT',
@@ -62,34 +61,34 @@ tickers = {
     'disney': 'DIS'
 
 }
-
 df = get_data(days, tickers)
-
 companies = st.multiselect(
-    '銘柄を選択してください。',
+    '会社名を選択してください。',
     list(df.index),
-    ['VYM', 'HDV', 'SPYD']
+    ['VYM', 'HDV', 'SPYD', 'VT']
 )
 
 if not companies:
-    st.error('少なくとも1銘柄は選んでください。')
+    st.error('少なくとも一社は選んでください。')
 else:
     data = df.loc[companies]
-    st.write("### 株価(USD)", data.sort_index())
+    st.write("### 株価 (USD)", data.sort_index())
     data = data.T.reset_index()
-    data = pd.melt(data, id_vars=['index']).rename(
-        columns={'value': 'stock prices(USD)', 'index': 'Date'}
+    data = pd.melt(data, id_vars=['Date']).rename(
+        columns={'value': 'Stock Prices(USD)'}
     )
     chart = (
         alt.Chart(data)
         .mark_line(opacity=0.8, clip=True)
         .encode(
             x="Date:T",
-            y=alt.Y("stock prices(USD):Q", stack=None,
+            y=alt.Y("Stock Prices(USD):Q", stack=None,
                     scale=alt.Scale(domain=[ymin, ymax])),
             color='Name:N'
         )
     )
     st.altair_chart(chart, use_container_width=True)
 # except:
-#     st.error("おっと！何かエラーが起きているようです・・・")
+#     st.error(
+#         "おっと！なにかエラーが起きているようです。"
+#     )
