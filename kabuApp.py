fd43c03c148f194461130a1cee4b0d3a18af4b60
moto95 <kobaesp1995@gmail.com>
2022-07-14 19:21:55 +0900

    整形

--- kabuApp.py
+++ kabuApp.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.title('米国株価可視化アプリ')
 
 st.sidebar.write("""
-# GAFA株価
+# 米国株価
 こちらは株価可視化ツールです。以下のオプションから表示日数を指定できます。
 """)
 
@@ -17,7 +17,7 @@ st.sidebar.write("""
 days = st.sidebar.slider('日数', 1, 700, 20)
 
 st.write(f"""
-### 過去 **{days}日間** のGAFA株価
+### 過去 **{days}日間** の米国株価
 """)
 
 
@@ -36,59 +36,59 @@ def get_data(days, tickers):
     return df
 
 
-# try:
-st.sidebar.write("""
-## 株価の範囲指定
-""")
-ymin, ymax = st.sidebar.slider(
-    '範囲を指定してください。',
-    0, 3000, (0, 200)
-)
-
-tickers = {
-    'VYM': 'VYM',
-    'VT': 'VT',
-    'VTI': 'VTI',
-    'SPYD': 'SPYD',
-    'HDV': 'HDV',
-    'VOO': 'VOO',
-    'apple': 'AAPL',
-    'meta': 'META',
-    'google': 'GOOGL',
-    'miclosoft': 'MSFT',
-    'netflix': 'NFLX',
-    'amazon': 'AMZN',
-    'disney': 'DIS'
-
-}
-df = get_data(days, tickers)
-companies = st.multiselect(
-    '会社名を選択してください。',
-    list(df.index),
-    ['VYM', 'HDV', 'SPYD', 'VT']
-)
-
-if not companies:
-    st.error('少なくとも一社は選んでください。')
-else:
-    data = df.loc[companies]
-    st.write("### 株価 (USD)", data.sort_index())
-    data = data.T.reset_index()
-    data = pd.melt(data, id_vars=['Date']).rename(
-        columns={'value': 'Stock Prices(USD)'}
+try:
+    st.sidebar.write("""
+    ## 株価の範囲指定
+    """)
+    ymin, ymax = st.sidebar.slider(
+        '範囲を指定してください。',
+        0, 3000, (0, 200)
     )
-    chart = (
-        alt.Chart(data)
-        .mark_line(opacity=0.8, clip=True)
-        .encode(
-            x="Date:T",
-            y=alt.Y("Stock Prices(USD):Q", stack=None,
-                    scale=alt.Scale(domain=[ymin, ymax])),
-            color='Name:N'
+
+    tickers = {
+        'VYM': 'VYM',
+        'VT': 'VT',
+        'VTI': 'VTI',
+        'SPYD': 'SPYD',
+        'HDV': 'HDV',
+        'VOO': 'VOO',
+        'apple': 'AAPL',
+        'meta': 'META',
+        'google': 'GOOGL',
+        'miclosoft': 'MSFT',
+        'netflix': 'NFLX',
+        'amazon': 'AMZN',
+        'disney': 'DIS'
+
+    }
+    df = get_data(days, tickers)
+    companies = st.multiselect(
+        '銘柄を選択してください。',
+        list(df.index),
+        ['VYM', 'HDV', 'SPYD', 'VT']
+    )
+
+    if not companies:
+        st.error('少なくとも一社は選んでください。')
+    else:
+        data = df.loc[companies]
+        st.write("### 株価 (USD)", data.sort_index())
+        data = data.T.reset_index()
+        data = pd.melt(data, id_vars=['Date']).rename(
+            columns={'value': 'Stock Prices(USD)'}
         )
+        chart = (
+            alt.Chart(data)
+            .mark_line(opacity=0.8, clip=True)
+            .encode(
+                x="Date:T",
+                y=alt.Y("Stock Prices(USD):Q", stack=None,
+                        scale=alt.Scale(domain=[ymin, ymax])),
+                color='Name:N'
+            )
+        )
+        st.altair_chart(chart, use_container_width=True)
+except:
+    st.error(
+        "おっと！なにかエラーが起きているようです。"
     )
-    st.altair_chart(chart, use_container_width=True)
-# except:
-#     st.error(
-#         "おっと！なにかエラーが起きているようです。"
-#     )
